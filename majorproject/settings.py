49e9956a907d21126a2b8ddf793ce13e4c9c334e
akumanuadithya123 <21bq1a4258@vvit.net>
2025-01-20 18:02:11 +0530

    update

--- majorproject/settings.py
+++ majorproject/settings.py
@@ -146,9 +146,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "login/static",
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static_files"
 
 
 
